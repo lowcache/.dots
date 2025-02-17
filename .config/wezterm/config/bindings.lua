5c1028b9a8d79949dcf27b3ebf0e757a5b5751bd
lowcache <drawpdeadredd@gmail.com>
2025-02-17 12:03:26 -0600

    wezterm bindings and .zshenv variable substitutions

--- .config/wezterm/config/bindings.lua
+++ .config/wezterm/config/bindings.lua
@@ -64,7 +64,7 @@ local keys = {
    -- tabs: spawn+close
    { key = 't',          mods = mod.SUPER,     action = act.SpawnTab('DefaultDomain') },
    { key = 't',          mods = mod.SUPER_REV, action = act.SpawnTab({ DomainName = 'WSL:Ubuntu' }) },
-   { key = 'w',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
+   { key = 'q',          mods = mod.SUPER_REV, action = act.CloseCurrentTab({ confirm = false }) },
 
    -- tabs: navigation
    { key = '[',          mods = mod.SUPER,     action = act.ActivateTabRelative(-1) },
@@ -88,7 +88,7 @@ local keys = {
       key = '-',
       mods = mod.SUPER,
       action = wezterm.action_callback(function(window, _pane)
-         local dimensions = window:get_dimensions()
+         local dimensions ssssssssss= window:get_dimensions()
          if dimensions.is_full_screen then
             return
          end
@@ -173,7 +173,7 @@ local keys = {
 
    -- panes: zoom+close pane
    { key = 'Enter', mods = mod.SUPER,     action = act.TogglePaneZoomState },
-   { key = 'w',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
+   { key = 'p',     mods = mod.SUPER,     action = act.CloseCurrentPane({ confirm = false }) },
 
    -- panes: navigation
    { key = 'k',     mods = mod.SUPER_REV, action = act.ActivatePaneDirection('Up') },
